c50cd5428102c420f9568f6c2b1601b602170edf
Saurabhi chaubey <chaubeysubhi@gmail.com>
2023-01-01 21:11:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 var x = window.matchMedia("(max-width: 500px)")
 var y = window.matchMedia("(min-width: 0px)")
 
+let slideIndex = 0;
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 2 seconds
+}
 
 function Open(){
 
+    if(x.matches &&  y.matches)
+    {
+   
+      document.getElementById("cutmenu").style.display="none";
+      document.getElementById("cutmenu3").style.display="none";
+      document.getElementById("cut1").style.display="block";
+    
+      document.getElementById("main1").style.display="none";  
+      document.getElementById("bodyi").style.backgroundColor="rgb(71, 216, 241)";
+    // document.getElementById("main1").style.position="fixed"; 
+    // document.getElementById("main1").style.overflow="hidden";
+    // document.getElementById("main1").style.paddingTop="0%";   
+      document.getElementById("bt").style.padding="100px 34px 0px 540px";    
+      document.getElementById("cut").style.width="0px";  
+      document.getElementById("cut").style.fontSize=0; 
+      document.getElementById("Sidebar").style.transion="0.4s"; 
+      document.getElementById("Sidebar").style.width="300px";
+    
+      document.getElementById("pr1").style.width="0px";
+      document.getElementById("pr1").style.height="0px";
+      document.getElementById("name1").style.padding="5% 10px 10% 20% ";
+    }
+    else{
     
     document.getElementById("cutmenu").style.display="none";
     document.getElementById("cutmenu3").style.display="none";
     document.getElementById("cut1").style.display="block";
     
-    document.getElementById("main1").style.float="right";    
+    document.getElementById("main1").style.float="right";  
+    document.getElementById("main1").style.position="fixed"; 
+    document.getElementById("main1").style.overflow="hidden";
+    document.getElementById("main1").style.paddingTop="0%";   
     document.getElementById("bt").style.padding="100px 34px 0px 540px";    
     document.getElementById("cut").style.width="0px";  
     document.getElementById("cut").style.fontSize=0; 
@@ -18,7 +58,10 @@ function Open(){
     
     document.getElementById("pr1").style.width="0px";
     document.getElementById("pr1").style.height="0px";
-    document.getElementById("name1").style.padding="17% 10px 10% 20% ";
+    document.getElementById("name1").style.padding="5% 10px 10% 20% ";
+    }
+    
+    
     
 
 }
@@ -28,7 +71,7 @@ function Close(){
     if(x.matches &&  y.matches)
     {
         document.getElementById("Sidebar").style.width="0px";
-  
+       
         document.getElementById("pr1").style.width="32%";
         document.getElementById("pr1").style.height="32%";
         document.getElementById("pr1").style.margin="32% 36% 0% 30%";
@@ -45,12 +88,16 @@ function Close(){
     }
     else{
     document.getElementById("Sidebar").style.width="0px";
+    document.getElementById("main1").style.overflow="visible";
   
-    document.getElementById("pr1").style.width="16%";
-    document.getElementById("pr1").style.height="16%";
-    document.getElementById("pr1").style.margin="2px 0px 0px 30px";
+  
+    document.getElementById("pr1").style.width="10%";
+    document.getElementById("pr1").style.height="10%";
+    document.getElementById("pr1").style.margin="2px 0px 0px 40px";
     document.getElementById("bt").style.padding="0px 0px 0px 48%"; 
     document.getElementById("name1").style.padding="2.3% 0px 0px 3px ";
+    document.getElementById("main1").style.overflow="scroll";
+    document.getElementById("main1").style.overflowX="hidden";
     document.getElementById("cut").style.fontSize="24px";
     }
     
@@ -84,13 +131,15 @@ function home(){
     document.getElementById("Menuresume").style.display="none";
     document.getElementById("main1").style.display="block";
     document.getElementById("cut").style.display="block";
-    document.getElementById("pr1").style.width="16%";
-    document.getElementById("pr1").style.height="16%";
-    document.getElementById("pr1").style.margin="2px 0px 0px 30px";
+    document.getElementById("pr1").style.width="10%";
+    document.getElementById("pr1").style.height="10%";
+    document.getElementById("pr1").style.margin="2px 0px 0px 40px";
     document.getElementById("bt").style.padding="0px 0px 0px 48%"; 
     document.getElementById("name1").style.padding="2.3% 0px 0px 3px ";
     document.getElementById("cut").style.fontSize="24px";
     document.getElementById("R1").style.opacity="1";
+    document.getElementById("main1").style.overflow="scroll";
+    document.getElementById("main1").style.overflowX="hidden";
     // document.getElementById("Sidebar").style.transition="0.4s";
   
     // document.getElementById("main1").style.display="block";
@@ -128,21 +177,22 @@ function  Resumefn(){
 
     if(x.matches && y.matches){
 
+        
         document.getElementById("Menuresume").style.display="none";
         document.getElementById("R1").style.display="none";
         document.getElementById("main1").style.display="none";
        
-        document.getElementById("bodyi").style.position="relative";
-        document.getElementById("bodyi").style.width="auto";
+        document.getElementById("bodyi").style.position="fixed";
+        document.getElementById("bodyi").style.overflowX="hidden";
+         document.getElementById("bodyi").style.backgroundColor="rgb(137, 201, 201)";
         document.getElementById("cutitr").style.display="block"; 
-        document.getElementById("bodyi").style.overflow="hidden";
-        document.getElementById("bodyi").style.backgroundColor="rgb(8, 17, 48)";
-        document.getElementById("bodyi").style.backgroundSize="cover";
-        document.getElementById("main1").style.display="none";
-        document.getElementById("Menuresume").style.display="none"
-        
-        document.getElementById("test").style.display="block"
-        document.getElementById("test").style.backgroundColor="rgb(8, 17, 48)"
+        // document.getElementById("bodyi").style.overflowX="hidden";
+        // document.getElementById("bodyi").style.backgroundColor="green";
+        // document.getElementById("bodyi").style.backgroundSize="cover";
+        document.getElementById("test").style.display="block";
+       document.getElementById("test").style.position="fixed";
+        document.getElementById("test").style.overflowX="hidden";
+        document.getElementById("test").style.backgroundColor="rgb(137, 201, 201)";
         document.getElementById("Sidebar").style.transition="0.4s";
         document.getElementById("Sidebar").style.width="0px";
       
@@ -156,12 +206,14 @@ function  Resumefn(){
     document.getElementById("R1").style.opacity="1";
     document.getElementById("R1").style.width="100%";
     document.getElementById("R1").style.height="100%";
+    document.getElementById("R1").style.display="flex";
     document.getElementById("Sidebar").style.transition="0.4s";
     document.getElementById("Sidebar").style.width="0px";
     document.getElementById("cutr").style.display="block";  
-    document.getElementById("Openmenu").style.display="block";;
+   // document.getElementById("Openmenu").style.display="block";;
     document.getElementById("Menuresume").style.display="none";
     document.getElementById("main1").style.display="none";
+    showSlides();
 
     if(document.getElementById("R1").style.opacity=="1")
     {
@@ -186,20 +238,20 @@ function Open2(){
     
     
     document.getElementById("Sidebar").style.width="22%";  
-    
+    document.getElementById("R1").style.display="none";
     document.getElementById("Sidebar").style.transition="0.4s";  
     document.getElementById("cut1").style.display="none"; 
-    document.getElementById("cutr").style.display="none"; 
-    document.getElementById("R1").style.width="0px"; 
+    document.getElementById("cutr").style.display="none";  
     document.getElementById("cutmenu").style.display="block";
-    document.getElementById("name1").style.padding="17% 10px 10% 20% ";
+    // document.getElementById("name1").style.padding="17% 10px 10% 20% ";
     
     // document.getElementById("Sidebar").style.width="300px";
     document.getElementById("Sidebar").style.position="fixed";
     document.getElementById("Sidebar").style.transition="0.4s";
     
-    document.getElementById("Openmenu").style.display="none";
-    document.getElementById("Menuresume").style.display="block";
+    // document.getElementById("Openmenu").style.display="none";
+    document.getElementById("Menuresume").style.display="flex";
+    document.getElementById("Menuresume").style.overflowX="hidden";
     document.getElementById("demo").style.paddingBottom="12px";
     document.getElementById("block3").style.display="block";
     document.getElementById("block3").style.padding="6% 0px 0px 0px ";
@@ -222,7 +274,6 @@ function Open3(){
    document.getElementById("Sidebar").style.transition="0.4s"; 
    document.getElementById("cutitr").style.display="none"; 
    document.getElementById("test").style.display="none";  
-   document.getElementById("mainenter").style.display="block";
    document.getElementById("cutmenu3").style.display="block";
    document.getElementById("cutr").style.display="none";
    document.getElementById("cut1").style.display="none";
@@ -239,15 +290,11 @@ function closerm(){
     document.getElementById("cutr").style.display="block";
     document.getElementById("cutmenu").style.display="none"; 
     document.getElementById("cutmenu3").style.display="none"; 
-    document.getElementById("R1").style.width="100%";
-    document.getElementById("R1").style.display="block";
-    document.getElementById("R1").style.height="100%"
-    document.getElementById("Openmenu").style.display="block";
-   
+    document.getElementById("R1").style.display="flex";   
     document.getElementById("Sidebar").style.width="0px";
    
     document.getElementById("main1").style.display="none";
-    if(document.getElementById("R1").style.display=="block")
+    if(document.getElementById("R1").style.display=="flex")
     {
         writer();
     }
